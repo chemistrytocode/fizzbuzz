280c30c221669ddc87ccbfc69e3f7a07fc2a5a32
Alex Hutchings <alexhutchings4@gmail.com>
2019-01-02 15:37:07 +0000

    updated to return number when passed number. added test for 6

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,32 +1,29 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-end
 
-describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-end
 
-describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
-end
 
-describe 'fizzbuzz' do
   it 'returns 1 when passed 1' do
     expect(fizzbuzz(1)).to eq 1
   end
 
-describe 'fizzbuzz' do
   it 'returns 2 when passed 2' do
     expect(fizzbuzz(2)).to eq 2
   end
-end
+
+  it 'returns "fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
 
 end
