d2d07be0c9368fdd43de421c9e3a6a2a713ff322
Paul <paul.ayling@bglgroup.co.uk>
2019-01-02 15:33:05 +0000

    Fixed fizzbuzz code and added rspec test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -22,4 +22,11 @@ describe 'fizzbuzz' do
   it 'returns 1 when passed 1' do
     expect(fizzbuzz(1)).to eq 1
   end
+
+describe 'fizzbuzz' do
+  it 'returns 2 when passed 2' do
+    expect(fizzbuzz(2)).to eq 2
+  end
+end
+
 end
